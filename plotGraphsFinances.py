3d3ed5b573a8a42454887dcf26e99c900b387493
Igor Santos <igorag314@gmail.com>
2020-12-02 23:33:12 -0300

    Gerar Grafico de Receita e Despesa do Ano Correspondendte

--- plotGraphsFinances.py
+++ plotGraphsFinances.py
@@ -1,4 +1,6 @@
 from pylab import *
+import numpy as np
+from random import randint
 
 class plotGraphs:
     
@@ -27,3 +29,33 @@ class plotGraphs:
 
         #EXIBIR GRAFICO
         show()
+
+    def generateGraphYear(self, revenue, spending):
+        
+        #DADOS DA AMOSTRAGEM
+        #REVENUE E SPENDING
+        months = ['JAN', 'FEV', 'MARC', 'ABR', 'MAI', 'JUN', 'JUL', 'AGO', 'SET', 'OUT', 'NOV', 'DEZ']
+
+        barWidth = 0.30
+        plt.figure(figsize=(10,5))
+
+        #POSICAO DAS BARRAS
+        r1 = np.arange(len(revenue))
+        r2 = [(x + barWidth) for x in r1]
+
+        #CRIANDO BARRAS
+        plt.bar(r1, revenue, color='Green', width=barWidth, label='REVENUE')
+        plt.bar(r2, spending, color='Red', width=barWidth, label='SPENGING')
+
+        #ADICIONANDO LEGENDAS AS BARRAS
+        plt.xlabel('MONTHS')
+        plt.xticks([r + barWidth for r in range(len(revenue))], months)
+        plt.ylabel('VALUE R$')
+        plt.title('GRAPH OF YEAR')
+
+        #CRIANDO E EXIBINDO O GRAFICO
+        plt.legend()
+        plt.show()
+
+p = plotGraphs()
+p.generateGraphYear([randint(0, 1000) for i in range(12)], [randint(0, 1000) for i in range(12)])
